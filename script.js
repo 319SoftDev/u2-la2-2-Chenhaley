04caaf8f65c35e23a23e03555e3d8001c0d4d423
Chenhaley <158762331+Chenhaley@users.noreply.github.com>
2024-10-24 10:40:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,69 @@
 console.log("script running");
 
 let totalInCents = 0;
+let items = 0;
+let appleCount = 0;
+let bananaCount = 0;
+let mangoCount = 0;
+let leImposter = 0;
 
 const appleButton = document.querySelector("#apple");
 const bananaButton = document.querySelector("#banana");
 const mangoButton = document.querySelector("#mango");
+const imposterButton = document.querySelector("#imposter");
 const shoppingCart = document.querySelector("#shopping-cart");
 const totalSpan = document.querySelector("#total-span");
+const itemize = document.querySelector("#item-counter");
 
 const clearButton = document.querySelector("#clear-button");
 
-console.log(appleButton, bananaButton, mangoButton, shoppingCart, totalSpan);
+console.log(appleButton, bananaButton, mangoButton, imposterButton, shoppingCart, totalSpan);
 
 const updateTotal = () => {
-  totalSpan.innerHTML = totalInCents / 100;
+  totalSpan.innerHTML = totalInCents/100;
 };
 
+const updateItems = () => {
+    itemize.innerHTML = items;
+    
+     items = appleCount;
+    // console.log(appleCount);
+};
+// console.log(appleCount + 1);
 const addApple = () => {
-  shoppingCart.innerHTML += `<p>Apple</p>`;
+ 
+  shoppingCart.innerHTML = `<li>Apple</li>`;
   totalInCents += 75;
+  appleCount += 1;
+  // console.log("Apple",appleCount);
   updateTotal();
 };
 appleButton.addEventListener("click", addApple);
 
 const addBanana = () => {
-  shoppingCart.innerHTML += `<p>Banana</p>`;
+  shoppingCart.innerHTML = `<li>Banana</li>`;
   totalInCents += 30;
+  bananaCount += 1;
   updateTotal();
 };
 bananaButton.addEventListener("click", addBanana);
 
 const addMango = () => {
-  shoppingCart.innerHTML += `<p>Mango</p>`;
+  shoppingCart.innerHTML = `<li>Mango</li>`;
   totalInCents += 125;
+  mangoCount += 1;
   updateTotal();
 };
 mangoButton.addEventListener("click", addMango);
+
+//imposter
+const addImposter = () => {
+  shoppingCart.innerHTML += `<p>Imposter</p>`;
+  totalInCents += 225;
+  leImposter += 1;
+  updateTotal();
+};
+imposterButton.addEventListener("click", addImposter);
 
 const emptyCart = () => {
   shoppingCart.innerHTML = '';
@@ -43,3 +71,8 @@ const emptyCart = () => {
   updateTotal();
 }
 clearButton.addEventListener("click", emptyCart);
+
+console.log(appleCount);  
+//p3
+// total.addEventListener("click", updateTotal());
+
